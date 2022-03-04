dbb9b8e05e1f758b44453889c5e81d5e93416538
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-03-04 03:32:46 +0200

    Move main() from scanner.l to al.cpp

--- lexical_analyser/al.cpp
+++ lexical_analyser/al.cpp
@@ -18,12 +18,21 @@ void showlist(std::list<struct alpha_token_t *> const a) {
     }
 }
 
-int main() {
-    int ntoken, vtoken;
-    ntoken = yylex();
-    while (ntoken) {
-        std::cout << ntoken << yytext; /*if(yylex() != COLON){ printf("syntax error ,expected colon but found %s\n ",yytext); return -1; }*/
-        ntoken = yylex();
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        if (!(yyin = fopen(argv[1], "r"))) {
+            fprintf(stderr, "Cannot read file: %s\n", argv[1]);
+            return -1;
+        }
+    } else {
+        yyin = stdin;
     }
+
+    alpha_token_t *token;
+
+    alpha_yylex(token);
+
+    showlist(token_list);
+
     return 0;
 }
