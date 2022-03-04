33e0fc660ac528ba5a519abe1ffe1888be7da2e7
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-03-04 12:04:09 +0200

    Support output redirection to file

--- lexical_analyser/al.cpp
+++ lexical_analyser/al.cpp
@@ -1,4 +1,5 @@
 #include "scanner.h"
+#include <fstream>
 #include <iostream>
 #include <list>
 #include <string>
@@ -8,31 +9,43 @@ extern int yylineno;
 extern char *yytext;
 extern FILE *yyin;
 
-void showlist(std::list<struct alpha_token_t *> const a) {
+void showlist(std::list<struct alpha_token_t *> const a, std::ostream &outfile) {
     for (struct alpha_token_t *x : a) {
-        std::cout << x->numline << " :"
-                  << "   #" << x->numToken
-                  << "   \"" << x->content << "\""
-                  << "   " << x->type
-                  << std::endl;
+        outfile << x->numline << " :"
+                << "   #" << x->numToken
+                << "   \"" << x->content << "\""
+                << "   " << x->type
+                << std::endl;
     }
 }
 
 int main(int argc, char **argv) {
+    std::ofstream outfile;
+
     if (argc > 1) {
         if (!(yyin = fopen(argv[1], "r"))) {
-            fprintf(stderr, "Cannot read file: %s\n", argv[1]);
+            std::cerr << "Cannot read file: " << argv[1] << std::endl;
             return -1;
         }
     } else {
         yyin = stdin;
     }
 
+    if (argc > 2) {
+        outfile.open(argv[2]);
+        if (!(outfile.is_open())) {
+            std::cerr << "Cannot open output file: " << argv[2] << std::endl;
+            return -1;
+        }
+    }
+
+    std::ostream &out = (argc > 2) ? outfile : std::cout;
+
     alpha_token_t *token;
 
     alpha_yylex(token);
 
-    showlist(token_list);
+    showlist(token_list, out);
 
     return 0;
 }
