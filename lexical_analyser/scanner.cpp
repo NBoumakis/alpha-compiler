b160c073be1907d031065222db2a0709dcf486e2
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-03-30 23:50:07 +0300

    Move scanner.h to header folder

--- lexical_analyser/scanner.cpp
+++ lexical_analyser/scanner.cpp
@@ -1,4 +1,4 @@
-#include "scanner.h"
+#include "../include/scanner.h"
 
 alpha_token_t::alpha_token_t(int line, int token, std::string content, std::string type)
     : numline(line), numToken(token), content(content), type(type) {}
