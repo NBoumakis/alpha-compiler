7140ab4730eb09d37f011ba006cebaf2df328f8c
Konstantina Papafragkaki <csd4331@csd.uoc.gr>
2022-03-31 19:27:34 +0300

    Add stmtValue

--- include/types.h
+++ include/types.h
@@ -14,3 +14,30 @@ struct programValue {
 
     programType valType;
 };
+
+enum stmtType {
+    EXPR,
+    IFSTMT,
+    WHILESTMT,
+    FORSTMT,
+    RETURNSTMT,
+    BLOCK,
+    FUNCDEF,
+    VOID
+};
+
+struct stmtValue {
+    union value {
+        struct exprValue *exprVal;
+        struct ifstmtValue *ifstmtVal;
+        struct whilestmtValue *whilestmtVal;
+        struct forstmtValue *forstmtVal;
+        struct returnstmtValue *returnstmtVal;
+        std::string BREAK;
+        std::string CONTINUE;
+        struct blockValue *blockVal;
+        struct funcdefValue *funcdefVal;
+    };
+
+    stmtType valType;
+};
