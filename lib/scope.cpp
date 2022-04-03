def1a5665c9b10c760789c9b727a6cd9f1cb1d79
Konstantina Papafragkaki <csd4331@csd.uoc.gr>
2022-04-03 20:19:08 +0300

    Add insert, hide functions

--- lib/scope.cpp
+++ lib/scope.cpp
@@ -1,2 +1,14 @@
 #include "../include/scope.h"
 #include "../include/symbol.h"
+
+void Scope::insert(std::string key, Symbol *symbol, unsigned int scope){
+    Scope::hash_table.insert({key,symbol});
+    Scope::scope_lists[scope].push_front(symbol);
+}
+    
+    
+void Scope::hide(unsigned int scopeLevel){
+    for (Symbol* symbol : Scope::scope_lists[scopeLevel]){
+        symbol->isActive=false;
+    }
+}
