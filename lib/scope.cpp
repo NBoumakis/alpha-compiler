f03992819ebeb1ce8d00c05438563e96500734d7
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-04-03 20:10:56 +0300

    scope lookup

--- lib/scope.cpp
+++ lib/scope.cpp
@@ -1,2 +1,17 @@
 #include "../include/scope.h"
 #include "../include/symbol.h"
+
+#include <list>
+#include <string>
+
+Symbol *Scope::lookup_scope(std::string key, unsigned int scope) {
+    std::list<Symbol *> scope_list = scope_lists[scope];
+
+    for (Symbol *it : scope_list) {
+        if (it->name == key) {
+            return it;
+        }
+    }
+
+    return nullptr;
+}
