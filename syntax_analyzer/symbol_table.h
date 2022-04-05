a96e44215ca53ae0c1d6748deec668d33da6d3d5
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-04-06 02:42:30 +0300

    Initialize static members

--- syntax_analyzer/symbol_table.h
+++ syntax_analyzer/symbol_table.h
@@ -5,11 +5,25 @@
 
 class Symbol_Table {
 public:
-    static Scope symbolTable;
+    static Scope symbolTableObj;
     static unsigned int scopeLevel;
     static std::unordered_set<std::string> libFunctions;
 
-    static bool isLibFunction(std::string name) {
-        return libFunctions.find(name) != libFunctions.end();
+    static inline bool isLibFunction(std::string name) {
+        return Symbol_Table::libFunctions.find(name) != Symbol_Table::libFunctions.end();
     }
 };
+
+unsigned int Symbol_Table::scopeLevel = 0;
+std::unordered_set<std::string> Symbol_Table::libFunctions = {"print",
+                                                              "input",
+                                                              "objectmemberkeys",
+                                                              "objecttotalmembers",
+                                                              "objectcopy",
+                                                              "totalarguments",
+                                                              "argument",
+                                                              "typeof",
+                                                              "strtonum",
+                                                              "sqrt", "cos", "sin"};
+
+Scope Symbol_Table::symbolTableObj();
