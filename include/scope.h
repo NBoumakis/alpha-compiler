7a25c049c531dc0082ebe7599d6fed6fbb29a865
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-04-06 12:26:53 +0300

    Get up-to-date with master

--- include/scope.h
+++ include/scope.h
@@ -1,3 +1,6 @@
+#ifndef __SCOPE_H
+#define __SCOPE_H
+
 #include "symbol.h"
 #include <list>
 #include <string>
@@ -7,17 +10,16 @@
 class Scope {
 private:
     std::unordered_multimap<std::string, Symbol *> hash_table;
-    std::vector<std::list<Symbol *>> scope_lists;
+    std::unordered_map<unsigned int, std::list<Symbol *>> scope_lists;
 
 public:
-    Scope();
-    ~Scope();
-
     void insert(std::string key, Symbol *symbol, unsigned int scope);
     void hide(unsigned int scopeLevel);
     Symbol *lookup_scope(std::string key, unsigned int scope);
-    Symbol *lookup_symbol(std::string key);
+    auto lookup_symbol(std::string key);
     /* Iterate over */
-    /* Remove element(key) */
-    /* Clear */
+    Symbol *erase(std::string key);
+    void clear();
 };
+
+#endif /* __SCOPE_H */
---
+++ include/symbol_table.h
@@ -0,0 +1,32 @@
+#ifndef __SYMBOL_TABLE_H
+#define __SYMBOL_TABLE_H
+
+#include "scope.h"
+#include "symbol.h"
+
+#include <unordered_set>
+
+class Symbol_Table {
+public:
+    static Scope symbolTableObj;
+    static unsigned int scopeLevel;
+    static std::unordered_set<std::string> libFunctions;
+
+    static inline bool isLibFunction(std::string name) {
+        return Symbol_Table::libFunctions.find(name) != Symbol_Table::libFunctions.end();
+    }
+};
+/*
+unsigned int Symbol_Table::scopeLevel = 0;
+std::unordered_set<std::string> Symbol_Table::libFunctions = {"print",
+                                                              "input",
+                                                              "objectmemberkeys",
+                                                              "objecttotalmembers",
+                                                              "objectcopy",
+                                                              "totalarguments",
+                                                              "argument",
+                                                              "typeof",
+                                                              "strtonum",
+                                                              "sqrt", "cos", "sin"};
+*/
+#endif /* __SYMBOL_TABLE_H */
