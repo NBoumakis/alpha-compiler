ca511044b350be17ad1ab3e4682650fd42cd7f53
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-04-20 12:22:43 +0300

    Convert to Savidis types

--- include/symbol.h
+++ include/symbol.h
@@ -4,11 +4,15 @@
 #include <string>
 
 enum SymbolType {
-    GLOBAL_VAR,
-    LOCAL_VAR,
-    FORMAL_ARG,
+    VARIABLE,
     USER_FUNC,
     LIB_FUNC
+};
+
+enum ScopespaceType {
+    GLOBAL_VAR,
+    LOCAL_VAR,
+    FORMAL_ARG
 };
 
 class Symbol {
@@ -18,6 +22,7 @@ public:
     unsigned int line;
     unsigned int funcDepth;
     bool isActive;
+
     SymbolType type;
 
     unsigned int offset;
@@ -25,8 +30,8 @@ public:
     friend class Scope;
 
 public:
-    Symbol(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type);
-    Symbol(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type, bool isActive);
+    Symbol(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type, unsigned offset);
+    Symbol(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type, unsigned offset, bool isActive);
 
     std::string to_string();
     ~Symbol();
@@ -34,15 +39,22 @@ public:
 
 class Variable : public Symbol {
 public:
-    Variable(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type) : Symbol(name, scope, line, funcDepth, type) {}
-    Variable(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type, bool isActive) : Symbol(name, scope, line, funcDepth, type, isActive) {}
+    ScopespaceType space;
+
+public:
+    Variable(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, ScopespaceType type, unsigned offset)
+        : Symbol(name, scope, line, funcDepth, VARIABLE, offset), space(type) {}
+    Variable(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, ScopespaceType type, unsigned offset, bool isActive)
+        : Symbol(name, scope, line, funcDepth, VARIABLE, offset, isActive), space(type) {}
 };
 
 class Function : public Symbol {
 protected:
 public:
-    Function(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type) : Symbol(name, scope, line, funcDepth, type) {}
-    Function(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type, bool isActive) : Symbol(name, scope, line, funcDepth, type, isActive) {}
+    Function(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type)
+        : Symbol(name, scope, line, funcDepth, type, offset) {}
+    Function(std::string name, unsigned int scope, unsigned int line, unsigned int funcDepth, SymbolType type, bool isActive)
+        : Symbol(name, scope, line, funcDepth, type, offset, isActive) {}
 };
 
 #endif /* __SYMBOL_H */
