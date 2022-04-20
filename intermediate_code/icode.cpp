c70186d798782f274e41f1cb87ad98d4eeb9da61
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-04-20 13:07:02 +0300

    Define emit

---
+++ intermediate_code/icode.cpp
@@ -0,0 +1,15 @@
+#include "icode.h"
+
+void emit(iopcode opcode, exprValue *arg1, exprValue *arg2, exprValue *result,
+          unsigned long label, unsigned long line) {
+    quad p;
+
+    p.opcode = opcode;
+    p.arg1 = arg1;
+    p.arg2 = arg2;
+    p.result = result;
+    p.line = line;
+    p.label = label;
+
+    quad_vector.push_back(p);
+}
