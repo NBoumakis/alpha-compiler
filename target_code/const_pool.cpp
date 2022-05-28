049a7f53fbbd28bfca176f77497b6bdd63c38568
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-05-29 01:37:04 +0300

    Functions return index of inserted element

--- target_code/const_pool.cpp
+++ target_code/const_pool.cpp
@@ -8,23 +8,23 @@ std::vector<Function *> userfunc_pool;
 unsigned long const_newstring(const std::string &str) {
     string_pool.push_back(str);
 
-    return string_pool.size();
+    return string_pool.size() - 1;
 }
 
 unsigned long const_newnumber(const double num) {
     number_pool.push_back(num);
 
-    return number_pool.size();
+    return number_pool.size() - 1;
 }
 
 unsigned long libfunc_newused(const std::string &name) {
     libfunc_pool.push_back(name);
 
-    return libfunc_pool.size();
+    return libfunc_pool.size() - 1;
 }
 
 unsigned long userfunc_newused(Function *const &func) {
     userfunc_pool.push_back(func);
 
-    return number_pool.size();
+    return number_pool.size() - 1;
 }
