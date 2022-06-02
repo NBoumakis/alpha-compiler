19f499b0637f5794b9118899a9f30ba14d369ede
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-02 23:28:07 +0300

    Target code: Generate executable: String

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "generators.h"
+#include "include/const_pool.h"
 #include "instruction.h"
 #include "scope.h"
 #include "scope_space.h"
@@ -32,6 +33,62 @@ char *getCmdOption(char **begin, char **end, const std::string &option) {
 
 bool cmdOptionExists(char **begin, char **end, const std::string &option) {
     return std::find(begin, end, option) != end;
+}
+
+unsigned long magic_number() {
+    return 340200501;
+}
+
+std::string const_strings() {
+    std::string res = std::to_string(string_pool.size()) + '\n';
+    for (auto &str : string_pool)
+        res += std::to_string(str.size()) + " " + str + "\n";
+
+    return res;
+}
+
+std::string const_numbers() {
+    std::string res = std::to_string(number_pool.size()) + '\n';
+    for (auto &num : number_pool)
+        res += std::to_string(num) + "\n";
+
+    return res;
+}
+
+std::string user_functions() {
+    std::string res = std::to_string(userfunc_pool.size()) + '\n';
+    for (auto func : userfunc_pool)
+        res += std::to_string(func->iaddress) + " " + std::to_string(func->totalLocals) + " " + func->name + "\n";
+
+    return res;
+}
+
+std::string lib_functions() {
+    std::string res = std::to_string(libfunc_pool.size()) + '\n';
+    for (auto &func : libfunc_pool)
+        res += std::to_string(func.size()) + " " + func + "\n";
+
+    return res;
+}
+
+std::string code() {
+    std::string res = std::to_string(instruction_vector.size()) + '\n';
+    for (auto inst : instruction_vector)
+        res += inst->to_string() + "\n";
+
+    return res;
+}
+
+void create_target(std::ofstream &out_file) {
+    generate_target_code();
+
+    out_file << magic_number() << std::endl
+             << std::endl;
+    out_file << const_strings() << std::endl;
+    out_file << const_numbers() << std::endl;
+    out_file << user_functions() << std::endl;
+    out_file << lib_functions() << std::endl;
+    out_file << code();
 }
 
 int main(int argc, char *argv[]) {
@@ -71,10 +128,18 @@ int main(int argc, char *argv[]) {
         if (cmdOptionExists(argv, argv + argc, "--quad"))
             out << quad_to_string();
 
-        generate_target_code();
+        char *bin_fname = getCmdOption(argv, argv + argc, "--target");
+        std::ofstream bin_file;
 
-        if (cmdOptionExists(argv, argv + argc, "--target"))
-            out << target_to_string();
+        if (bin_fname) {
+            bin_file.open(bin_fname);
+            if (!(bin_file.is_open())) {
+                std::cerr << "Cannot open output for binary file: " << foutput << std::endl;
+                return -1;
+            }
+
+            create_target(bin_file);
+        }
     } else {
         out << "Compilation failed. " << comp_err << " errors found." << std::endl;
     }
