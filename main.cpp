6d471553a29debc7ba0ca49063f036dd5e0d4233
NIKOLAOS BOUMAKIS <csd4346@csd.uoc.gr>
2022-06-10 15:43:10 +0300

    Ability to generate binary files

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ std::string const_numbers() {
 std::string user_functions() {
     std::string res = std::to_string(userfunc_pool.size()) + '\n';
     for (auto func : userfunc_pool.get_straight())
-        res += std::to_string(func.second->iaddress) + " " +
+        res += std::to_string(func.second->taddress) + " " +
                std::to_string(func.second->totalLocals) + " " +
                std::to_string(func.second->name.size()) + " " +
                func.second->name + "\n";
@@ -95,9 +95,7 @@ unsigned long global_count() {
     return count;
 }
 
-void create_target(std::ofstream &out_file) {
-    generate_target_code();
-
+void create_text_target(std::ofstream &out_file) {
     out_file << magic_number() << std::endl
              << std::endl;
     out_file << global_count() << std::endl
@@ -107,6 +105,94 @@ void create_target(std::ofstream &out_file) {
     out_file << user_functions() << std::endl;
     out_file << lib_functions() << std::endl;
     out_file << code();
+}
+
+void create_binary_target(std::ofstream &out_file) {
+    unsigned long ulong = magic_number();
+    out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+
+    ulong = global_count();
+    out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+
+    ulong = string_pool.size();
+    out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+    for (auto &str : string_pool.get_straight()) {
+        ulong = str.second.size();
+        out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+        out_file.write(str.second.c_str(), str.second.size());
+    }
+
+    ulong = number_pool.size();
+    out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+    for (auto &num : number_pool.get_straight()) {
+        double double_const = num.second;
+        out_file.write(reinterpret_cast<char *>(&double_const), sizeof(double_const));
+    }
+
+    ulong = userfunc_pool.size();
+    out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+    for (auto &ufunc : userfunc_pool.get_straight()) {
+        ulong = ufunc.second->taddress;
+        out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+
+        ulong = ufunc.second->totalLocals;
+        out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+
+        ulong = ufunc.second->name.size();
+        out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+        out_file.write(ufunc.second->name.c_str(), ufunc.second->name.size());
+    }
+
+    ulong = libfunc_pool.size();
+    out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+    for (auto &libfunc : libfunc_pool.get_straight()) {
+        ulong = libfunc.second.size();
+        out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+
+        out_file.write(libfunc.second.c_str(), libfunc.second.size());
+    }
+
+    ulong = instruction_vector.size();
+    out_file.write(reinterpret_cast<char *>(&ulong), sizeof(ulong));
+    for (auto inst : instruction_vector) {
+        out_file.write(reinterpret_cast<char *>(&inst->opcode), sizeof(inst->opcode));
+
+        if (inst->arg1) {
+            out_file.write(reinterpret_cast<char *>(&inst->arg1->type), sizeof(inst->arg1->type));
+        } else {
+            // Possible bug
+            unsigned long invType = -1;
+            out_file.write(reinterpret_cast<char *>(&invType), sizeof(vmarg_t));
+        }
+
+        if (inst->arg2) {
+            out_file.write(reinterpret_cast<char *>(&inst->arg2->type), sizeof(inst->arg2->type));
+        } else {
+            // Possible bug
+            unsigned long invType = -1;
+            out_file.write(reinterpret_cast<char *>(&invType), sizeof(vmarg_t));
+        }
+
+        if (inst->result) {
+            out_file.write(reinterpret_cast<char *>(&inst->result->type), sizeof(inst->result->type));
+        } else {
+            // Possible bug
+            unsigned long invType = -1;
+            out_file.write(reinterpret_cast<char *>(&invType), sizeof(vmarg_t));
+        }
+
+        if (inst->arg1) {
+            out_file.write(reinterpret_cast<char *>(&inst->arg1->val), sizeof(inst->arg1->val));
+        }
+
+        if (inst->arg2) {
+            out_file.write(reinterpret_cast<char *>(&inst->arg2->val), sizeof(inst->arg2->val));
+        }
+
+        if (inst->result) {
+            out_file.write(reinterpret_cast<char *>(&inst->result->val), sizeof(inst->result->val));
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -150,13 +236,25 @@ int main(int argc, char *argv[]) {
         std::ofstream bin_file;
 
         if (bin_fname) {
-            bin_file.open(bin_fname);
-            if (!(bin_file.is_open())) {
-                std::cerr << "Cannot open output for binary file: " << foutput << std::endl;
-                return -1;
+            generate_target_code();
+
+            if (cmdOptionExists(argv, argv + argc, "--binary")) {
+                bin_file.open(bin_fname, std::ios::out | std::ios::binary);
+                if (!(bin_file.is_open())) {
+                    std::cerr << "Cannot open output for binary file: " << foutput << std::endl;
+                    return -1;
+                }
+                create_binary_target(bin_file);
+            } else {
+                bin_file.open(bin_fname);
+                if (!(bin_file.is_open())) {
+                    std::cerr << "Cannot open output for text file: " << foutput << std::endl;
+                    return -1;
+                }
+                create_text_target(bin_file);
             }
 
-            create_target(bin_file);
+            bin_file.close();
         }
     } else {
         out << "Compilation failed. " << comp_err << " errors found." << std::endl;
